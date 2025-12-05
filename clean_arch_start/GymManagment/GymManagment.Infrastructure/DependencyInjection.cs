f2c6b528f4f23865e7904ad2d9b6f5cdf49dc613
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-05 13:52:14 +0500

    Добавлены репозитории для Admins, Gyms в DependencyInjection в инфраструктуре

--- clean_arch_start/GymManagment/GymManagment.Infrastructure/DependencyInjection.cs
+++ clean_arch_start/GymManagment/GymManagment.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using GymManagment.Application.Common.Interfaces;
+using GymManagment.Infrastructure.Admins.Persistence;
 using GymManagment.Infrastructure.Common.Persistence;
+using GymManagment.Infrastructure.Gyms.Persistence;
 using GymManagment.Infrastructure.Subscriptions.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,11 +12,18 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        return services.AddPersistence();
+    }
+
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
         services.AddDbContext<GymManagementDbContext>(options =>
         {
             options.UseSqlite("Data Source = GymManagement.db");
         });
         services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
+        services.AddScoped<IAdminsRepository, AdminsRepository>();
+        services.AddScoped<IGymsRepository, GymsRepository>();
         services.AddScoped<IUnitOfWork>(serviceProvider =>
             serviceProvider.GetRequiredService<GymManagementDbContext>());
         return services;
