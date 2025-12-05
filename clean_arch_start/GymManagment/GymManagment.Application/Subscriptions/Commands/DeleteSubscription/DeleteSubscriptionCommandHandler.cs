09b69271bb2e705a5326c3e834129fa7b86d6ff0
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-05 20:39:34 +0500

    22-24. Создание предметно-ориентированного события

--- clean_arch_start/GymManagment/GymManagment.Application/Subscriptions/Commands/DeleteSubscription/DeleteSubscriptionCommandHandler.cs
+++ clean_arch_start/GymManagment/GymManagment.Application/Subscriptions/Commands/DeleteSubscription/DeleteSubscriptionCommandHandler.cs
@@ -4,25 +4,28 @@ using MediatR;
 
 namespace GymManagment.Application.Subscriptions.Commands.DeleteSubscription;
 
-public class DeleteSubscriptionCommandHandler: IRequestHandler<DeleteSubscriptionCommand, ErrorOr<Deleted>>
+public class DeleteSubscriptionCommandHandler : IRequestHandler<DeleteSubscriptionCommand, ErrorOr<Deleted>>
 {
     private readonly ISubscriptionsRepository _subscriptionsRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IAdminsRepository _adminsRepository;
-    private readonly IGymsRepository _gymsRepository;
+    // private readonly IGymsRepository _gymsRepository;
 
-    public DeleteSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork, IAdminsRepository adminsRepository, IGymsRepository gymsRepository)
+    public DeleteSubscriptionCommandHandler(
+        ISubscriptionsRepository subscriptionsRepository,
+        IUnitOfWork unitOfWork,
+        IAdminsRepository adminsRepository
+    )
     {
         _subscriptionsRepository = subscriptionsRepository;
         _unitOfWork = unitOfWork;
         _adminsRepository = adminsRepository;
-        _gymsRepository = gymsRepository;
     }
 
     public async Task<ErrorOr<Deleted>> Handle(DeleteSubscriptionCommand command, CancellationToken cancellationToken)
     {
         var subscription = await _subscriptionsRepository.GetByIdAsync(command.SubscriptionId);
-        if(subscription is null)
+        if (subscription is null)
         {
             return Error.NotFound("Subscription not found");
         }
@@ -30,17 +33,17 @@ public class DeleteSubscriptionCommandHandler: IRequestHandler<DeleteSubscriptio
         var admin = await _adminsRepository.GetByIdAsync(subscription.AdminId);
         if (admin is null)
         {
-            return Error.Unexpected(description:"Admin not found");
+            return Error.Unexpected(description: "Admin not found");
         }
-        
+
         admin.DeleteSubscription(command.SubscriptionId);
-        var gymsToDelete = await _gymsRepository.ListBySubscriptionIdAsync(command.SubscriptionId);
-        
+        // var gymsToDelete = await _gymsRepository.ListBySubscriptionIdAsync(command.SubscriptionId);
+
         await _adminsRepository.UpdateAsync(admin);
-        await _subscriptionsRepository.RemoveSubscriptionAsync(subscription);
-        await _gymsRepository.RemoveRangeAsync(gyms: gymsToDelete);
+        // await _subscriptionsRepository.RemoveSubscriptionAsync(subscription);
+        // await _gymsRepository.RemoveRangeAsync(gyms: gymsToDelete);
         await _unitOfWork.CommitChangesAsync();
-        
+
         return Result.Deleted;
     }
 }
