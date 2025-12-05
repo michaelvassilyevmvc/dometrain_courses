fbed8ae48adff2b7538ad36a1dd6750e766959cc
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-05 09:30:28 +0500

    Для Domain созданы Admin, Gym, Room. Добавлена rich модель для Subscription

---
+++ clean_arch_start/GymManagment/GymManagment.Domain/Admins/Admin.cs
@@ -0,0 +1,40 @@
+﻿using GymManagment.Domain.Subscriptions;
+using Throw;
+
+namespace GymManagment.Domain.Admins;
+
+public class Admin
+{
+    private Admin()
+    {
+    }
+
+    public Admin(
+        Guid userId,
+        Guid? subscriptionId = null,
+        Guid? id = null
+        )
+    {
+        Id = id ?? Guid.NewGuid();
+        SubscriptionId = subscriptionId;
+        UserId = userId;
+    }
+
+    public Guid Id { get; private set; }
+    public Guid? SubscriptionId { get; private set; } = null;
+    public Guid UserId { get; }
+
+    public void SetSubscription(Subscription subscription)
+    {
+        SubscriptionId.HasValue.Throw()
+            .IfTrue();
+        SubscriptionId = subscription.Id;
+    }
+    
+    public void DeleteSubscription(Guid subscriptionId)
+    {
+        SubscriptionId.ThrowIfNull()
+            .IfNotEquals(subscriptionId);
+        SubscriptionId = null;
+    }
+}
