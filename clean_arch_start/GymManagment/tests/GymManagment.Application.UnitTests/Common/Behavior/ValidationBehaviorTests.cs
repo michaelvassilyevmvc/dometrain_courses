cb8a950a5ba342cae64a3f7cb2479bc1a76cd051
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-06 15:23:20 +0500

    35-36. Создания тестов для проверки ValidationBehavior. Создание проектка тестов для проверки Application логики

---
+++ clean_arch_start/GymManagment/tests/GymManagment.Application.UnitTests/Common/Behavior/ValidationBehaviorTests.cs
@@ -0,0 +1,60 @@
+﻿using GymManagment.Application.Common.Behaviors;
+using GymManagment.Application.Gyms.Commands.CreateGym;
+using GymManagment.Domain.Gyms;
+using ErrorOr;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using NSubstitute;
+using TestCommon.Gyms;
+
+namespace GymManagment.Application.UnitTests.Common.Behavior;
+
+public class ValidationBehaviorTests
+{
+    private readonly RequestHandlerDelegate<ErrorOr<Gym>> _mockNextBehavior;
+    private readonly IValidator<CreateGymCommand> _mockValidator;
+    private readonly ValidationBehavior<CreateGymCommand, ErrorOr<Gym>> _validationBehavior;
+
+    public ValidationBehaviorTests()
+    {
+        
+
+        // Создадим следующее поведение
+        _mockNextBehavior = Substitute.For<RequestHandlerDelegate<ErrorOr<Gym>>>();
+        
+        _mockValidator = Substitute.For<IValidator<CreateGymCommand>>();
+        
+        // Создадим валидаток (mock)
+        // Создадим поведение валидации
+        _validationBehavior = new ValidationBehavior<CreateGymCommand, ErrorOr<Gym>>(_mockValidator);
+    }
+    
+    [Fact]
+    public async Task InvokeBehavior_WhenValidatorResultIsValid_ShouldInvokeNextBehavior()
+    {
+        // Arrange
+        // Создать запрос
+        var createGymRequest = GymCommandFactory.CreateGymCommand();
+        var gym = GymFactory.CreateGym();
+        
+        _mockValidator.ValidateAsync(createGymRequest, Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult());
+        
+        _mockNextBehavior.Invoke()
+            .Returns(gym);
+
+        // Act
+        // Вызов поведения
+        var result = await _validationBehavior.Handle(createGymRequest, _mockNextBehavior, CancellationToken.None);
+
+        // Assert
+        // Результат вызова поведения
+        // Был результат следующего поведения
+        result.IsError.Should()
+            .BeFalse();
+        result.Value.Should()
+            .BeEquivalentTo(gym);
+    }
+}
---
+++ clean_arch_start/GymManagment/tests/TestCommon/Gyms/GymCommandFactory.cs
@@ -0,0 +1,14 @@
+﻿using GymManagment.Application.Gyms.Commands.CreateGym;
+using TestCommon.TestContants;
+
+namespace TestCommon.Gyms;
+
+public static class GymCommandFactory
+{
+    public static CreateGymCommand CreateGymCommand(
+        string name = Contants.Gym.Name,
+        Guid? subscriptionId = null)
+    {
+        return new CreateGymCommand(name, subscriptionId ?? Contants.Subscriptions.Id);
+    }
+}
