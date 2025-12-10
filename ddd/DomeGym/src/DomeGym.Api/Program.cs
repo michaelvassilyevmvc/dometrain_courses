7bef89fe5588b7a44e47766b226af460506925aa
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-12-10 13:41:44 +0500

    Section5. Добавлены проекты для Contracts, Infrastructure, Api

---
+++ ddd/DomeGym/src/DomeGym.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+builder.Services.AddProblemDetails();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
